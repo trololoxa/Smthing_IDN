5b241a9008dc3f4bcb808ac5e931f39b8b7386d3
Trololoxa221 <nikolai.2006-2015@yandex.ru>
2021-12-05 00:01:34 +0300

    JSON Load

--- TextVersionPackage/Save_Load.py
+++ TextVersionPackage/Save_Load.py
@@ -1,7 +1,7 @@
 from Player import Player
 import os
 import json
-#import hashlib
+# import hashlib
 
 save_folder = os.getenv('APPDATA') + "\\..\\Kinda Hacker Game\\Saves\\"
 if not os.path.exists(save_folder):
@@ -9,28 +9,28 @@ if not os.path.exists(save_folder):
 save_files = os.listdir(save_folder)
 
 
+# TODO: Test Json save & load
 def save(inp):
     money = inp[1]
     pc_tier = inp[2]
-    #TODO: encrypt/decrypt save
-    #TODO: Json save & load
-    with open(save_folder+inp[0], 'w') as json_file:
-        data = {'save': [{'money': inp[1], 'pc_tier': inp[2]}]}
+    # TODO: encrypt/decrypt save
+    with open(save_folder + inp[0], 'w') as json_file:
+        data = {'save': [{'savelen': 2, 'money': inp[1], 'pc_tier': inp[2]}]}
         json.dump(data, json_file)
 
 
 def load(is_new=True):
-    #TODO: cls
-    #os.system("cls")
+    # TODO: cls
+    # os.system("cls")
     if is_new:
         Sanya = Player()
         save(Sanya.play_game())
     else:
         print("Plz choose save file")
         for i in range(len(save_files)):
-            print(str(i+1)+") "+save_files[i])
+            print(str(i + 1) + ") " + save_files[i])
         while True:
-            #TODO: Lowercase
+            # TODO: Lowercase
             print("print:", end=" ")
             command = input()
             try:
@@ -38,19 +38,32 @@ def load(is_new=True):
                     loadfile = command
                     break
                 elif len(save_files) >= int(command) > 0:
-                    loadfile = save_files[int(command)-1]
+                    loadfile = save_files[int(command) - 1]
                     break
                 else:
                     print("Wrong input")
             except ValueError:
                 print("plz input number or name of savefile u want load")
-        load_file(save_folder+loadfile, loadfile)
+        load_file(save_folder + loadfile, loadfile)
 
-#TODO: name save files
+
+# TODO: name save files
 def load_file(file_loc, file_name):
-    with open(file_loc, 'r') as file:
-        money = int(file.readline())
-        pc_tier = int(file.readline())
-    #TODO: Send save file name
+    try:
+        with open(file_loc, 'r') as file:
+            ld = json.load(file)
+            savedata = ld['save'][0]
+            if savedata['savelen'] != 2:
+                print('Old save file format')
+                exit(-1)
+            money = savedata['money']
+            pc_tier = savedata['pc_tier']
+    except KeyError:
+        print('Incorrect save file format1')
+        raise exit(-1)
+    except json.decoder.JSONDecodeError:
+        print('Incorrect save file format2')
+        raise exit(-1)
+    # TODO: Send save file name
     Sanya = Player(money=money, pc_tier=pc_tier)
     save(Sanya.play_game())
