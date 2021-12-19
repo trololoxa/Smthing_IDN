60e04dee0a3b7a96eb1cc39fc9744cb1cf42f821
Lesson Pc(Trololoxa221) <nikolai.2006-2015@yandex.ru>
2021-12-19 15:58:33 +0300

    2 Ursina

--- main.py
+++ main.py
@@ -3,55 +3,26 @@ from ursina import *
 import random
 
 rand_gen = random.Random()
-texoffset = 0.0
 
 
 def update():
-    cube.rotation_y += rand_gen.random() * time.dt * 100 * 0
-    cube.rotation_x += rand_gen.random() * time.dt * 100 * 0
-    cube.rotation_z += rand_gen.random() * time.dt * 100 + 1
+    pass
 
-    if held_keys['r']:
-        red = rand_gen.random() * 255
-        green = rand_gen.random() * 255
-        blue = rand_gen.random() * 255
-        cube.color = color.rgb(red, green, blue)
-
-    global texoffset
-    texoffset += time.dt * 0.2
-    setattr(cube, "texture_offset", (0, texoffset))
-
-    if mouse.hovered_entity == cube and mouse.left == True:
-        info.visible = True
-    else:
-        info.visible = False
 
 def input(key):
-    if key == 'space':
-        red = rand_gen.random() * 255
-        green = rand_gen.random() * 255
-        blue = rand_gen.random() * 255
-        cube.color = color.rgb(red, green, blue)
-
+    pass
 
 app = Ursina()
 
 window.title = 'My Gay'
 window.borderless = False
-window.fullscreen = False
+window.forced_aspect_ratio = (16,9)
+window.windowed_size = (1280, 720)
 window.exit_button.visible = False
 window.fps_counter.enabled = True
 
-cubes = []
-cube = Entity(model='cube', color=color.orange, scale=(6,6,0.1), texture="waterfall", collider="box")
-cubes.append(cube)
-
-Text.size = 0.05
-Text.default_resolution = 1080 * Text.size
-info = Text(text="A powerful waterfall roaring on the mountains")
-info.x = -0.5
-info.y = 0.4
-info.background = True
-info.visible = False
+b = Button(text='Yeaa boi', color=color.black, scale=.25)
+b.position = (-0.75,0.35,0)
+b.on_click = application.quit
 
 app.run()
